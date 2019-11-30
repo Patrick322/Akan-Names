70cbef082a96069480a8b328bf5fef2582d35cdf
Patrick595 <patrickbuong@gmail.com>
2019-11-30 13:42:14 +0300

    it will add some funcions in js

---
+++ js/script.js
@@ -0,0 +1,21 @@
+var akanArray = [
+    {
+        "Sunday": "Kwasi",
+        "Monday": "Kwadwo",
+        "Tuesday": "Kwabena",
+        "Wednesday": "Kwaku",
+        "Thurday": "Yaw",
+        "Friday": "Kofi",
+        "Saturday": "Kwame"
+    },
+    {
+        "Sunday": "Akosua",
+        "Monday": "Adwoa",
+        "Tuesday": "Abenaa",
+        "Wednesday": "Akua",
+        "Thurday": "Yaa",
+        "Friday": "Afua",
+        "Saturday": "Ama"
+
+    }
+]
